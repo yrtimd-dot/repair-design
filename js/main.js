e3638d33352eecce9dc95bd0f60492bfa665b69b
dima <reyser5858@gmail.com>
2020-03-17 18:07:44 +0900

    feat: validation + footer block

--- js/main.js
+++ js/main.js
@@ -45,4 +45,38 @@ $(document).ready(function () {
   bullets.css('left', prev.width() + 10)
 
   new WOW().init();
+
+  // Валидация формы
+  $('.modal__form').validate({
+    errorClass: "invalid",
+    rules: {
+      // строчное правило
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: "required",
+      // правило-объект (блок)
+      userEmail: {
+        required: true,
+        email: true
+      }
+    }, // сообщения 
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя не короче двух букв"
+      },
+      userPhone: "Телефон обязателен",
+      userEmail: {
+        required: "Обязательно укажите email",
+        email: "Введите в формате: name@domain.com"
+      }
+    }
+  });
+
+  // маска для телефона
+
+  $('[type=tel]').mask('+7(000) 00-00-000', {placeholder: "+7 (___) __-__-___"});
+
 });
